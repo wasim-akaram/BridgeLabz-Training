8bb89366dae570f2c58efd02d703e8539a7cab7b
wasim-akaram <wasimakaram360@gmail.com>
2025-12-18 17:03:14 +0530

    [Wasim] : added Continuous non negative sum program until input is zero

---
+++ gcr-codebase/control-flows/LevelOne/ContinuousNonNegativeSum.java
@@ -0,0 +1,29 @@
+//program to find the sum of numbers until the user enters 0 or negative number
+
+import java.util.*;
+public class ContinuousNonNegativeSum
+{
+	public static void main(String args[])
+	{
+		//creating object of scanner
+		Scanner input= new Scanner(System.in);
+		
+		//asking for input
+		System.out.println("Enter the numbers to be added");
+		
+		
+		//creating variable for initializing loop an sum
+		int current=1 , sum=0;
+		
+		//taking input in while loop and adding
+		while (current >0)
+		{
+		int number= input. nextInt();
+		current=number;
+		sum= sum + current;
+		}
+		System.out.println("Total sum is " + sum);
+		
+	}
+}
+		
