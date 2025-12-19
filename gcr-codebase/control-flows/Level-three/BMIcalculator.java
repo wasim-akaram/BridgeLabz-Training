d7f48c7119265ace1db9e7ed243149bad94bdf83
wasim-akaram <wasimakaram360@gmail.com>
2025-12-19 15:45:23 +0530

    [Wasim] : added BMI calculator program

---
+++ gcr-codebase/control-flows/Level-three/BMIcalculator.java
@@ -0,0 +1,45 @@
+//program to find the BMI of a person
+
+
+import java.util.Scanner;
+public class BMIcalculator
+{
+	public static void main(String []args)
+	{
+		// Create a Scanner Object
+		Scanner sc=new Scanner(System.in);
+		
+		//taking input
+		System.out.println("Enter the weight in kg");
+		double weight = sc.nextDouble();
+		System.out.println("Enter the height in cm");
+		double height = sc.nextDouble();
+		
+		// convert height to meter
+		height=height/100.0;
+		
+		
+		//calculating BMI
+		double bmi= weight / (height * height);
+		
+		//displaying status
+		if( bmi >=40)
+		{
+			System.out.println("Status : Obese");
+		}
+		else if( bmi >= 25 && bmi < 40)
+		{
+			System.out.println("Status : Overweight");
+		}
+		else if( bmi >= 18.5 && bmi < 25)
+		{
+			System.out.println("Status : Normal");
+		}
+		else if( bmi >= 18.4)
+		{
+			System.out.println("Status : Underweight");
+		}
+	}
+}
+		
+		
