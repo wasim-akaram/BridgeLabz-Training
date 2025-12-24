5e0d1dce8abbdadd861da7afb8423fc9156dba57
wasim-akaram <wasimakaram360@gmail.com>
2025-12-24 12:16:34 +0530

    [Wasim] : added program to reverse a string

--- gcr-codebase/Extras/Extra-String/ReversedString.java
+++ gcr-codebase/Extras/Extra-String/ReversedString.java
@@ -9,11 +9,11 @@ public class ReversedString
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter a string:");
         String str=sc.nextLine();
-        String rev="";
+        String reversed="";
         for(int i=str.length()-1;i>=0;i--)
         {
-            rev=rev+str.charAt(i);
+            reversed=reversed+str.charAt(i);
         }
-        System.out.println("Reversed string is:"+rev);
+        System.out.println(reversed);
     }
 }
