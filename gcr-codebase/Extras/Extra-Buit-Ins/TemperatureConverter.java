74b59f517975526ae0d0978944f858c5037444c9
wasim-akaram <wasimakaram360@gmail.com>
2025-12-25 17:27:52 +0530

    [Wasim] : added program to Convert temperature

---
+++ gcr-codebase/Extras/Extra-Buit-Ins/TemperatureConverter.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+public class TemperatureConverter
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		double temp = sc.nextDouble();
+		
+		System.out.println("Celecius to fahrenheit "+fahrenheitToCelsius(temp));
+		System.out.println("Fahrenheit to celsius "+celsiusToFahrenheit(temp));
+	}
+	 
+	public static double fahrenheitToCelsius(double f)
+	{
+        return (f - 32) * 5 / 9;
+    }
+
+    public static double celsiusToFahrenheit(double c)
+	{
+        return (c * 9 / 5) + 32;
+    }
+}
