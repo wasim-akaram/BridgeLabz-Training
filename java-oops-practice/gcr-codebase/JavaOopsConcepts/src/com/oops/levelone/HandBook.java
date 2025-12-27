815f4e747f0f4d26ef53c5a00dae56a6d805c3d2
wasim-akaram <wasimakaram360@gmail.com>
2025-12-27 17:52:55 +0530

    [Wasim] : added the program for Handbook

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/oops/levelone/HandBook.java
@@ -0,0 +1,32 @@
+package com.oops.levelone;
+
+public class HandBook 
+{
+	//Declaring the variables to get the details of hand book
+		String bookTitle;
+		String bookAuthor;
+		double price;
+		
+		//constructor
+		HandBook(String bookTitle,String bookAuthor,double price){
+			this.bookTitle=bookTitle;
+			this.bookAuthor=bookAuthor;
+			this.price=price;
+		}
+		
+		//creating the method to display the output
+		public void displayBookDetails() {
+			System.out.println("Title of the book: "+bookTitle);
+			System.out.println("Author of the book: "+bookAuthor);
+			System.out.println("Price of the book: "+price);
+		}
+		
+		public static void main(String[] args) {
+		HandBook book1= new HandBook("2States", "Chetan Bhagat", 500);
+		HandBook book2=new HandBook("Wings Of Fire","Abdul kalam.A.P.J",  500);
+		book1.displayBookDetails();
+		book2.displayBookDetails();
+		}
+
+
+}
