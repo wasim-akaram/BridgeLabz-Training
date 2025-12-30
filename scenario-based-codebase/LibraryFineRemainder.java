05a8c4fbcf3cfeea55df1636c668b921f578f6a7
wasim-akaram <wasimakaram360@gmail.com>
2025-12-30 17:00:09 +0530

    [Wasim] : added library fine remainder program

---
+++ scenario-based-codebase/LibraryFineRemainder.java
@@ -0,0 +1,40 @@
+/* Rohan’s Library Reminder App 📚
+Rohan wants a fine calculator:
+● Input return date and due date.
+● If returned late, calculate fine: ₹5/day.
+● Repeat for 5 books using for-loop. */
+
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
+public class LibraryFineRemainder
+{
+	public static void main (String args[])
+	{
+		for(int i=0; i<5; i++)
+		{
+			findFine();
+		}
+		
+	}
+	
+	//method to calculate fine
+	public static void findFine()
+	{
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Enter the return date (dd-mm-yyyy): ");
+		String returnDate = sc.nextLine();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+		LocalDate date1 = LocalDate .parse(returnDate, formatter);
+		
+			
+			System.out.println("Enter the due date (dd-mm-yyyy): ");
+		String dueDate = sc.nextLine();
+		LocalDate dueDate2 = LocalDate.parse(dueDate, formatter);
+		
+		long totalDays = ChronoUnit.DAYS.between(date1, dueDate2);
+		System.out.println("Total fine is Rs: " + totalDays*5);
+	}
+}
