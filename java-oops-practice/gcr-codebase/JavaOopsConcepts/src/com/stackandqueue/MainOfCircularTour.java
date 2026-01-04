a4ae931c8c1a86b5aa15372e35ba84c1eb461f6e
wasim-akaram <wasimakaram360@gmail.com>
2026-01-05 01:28:51 +0530

    [Wasim] : added circular tour problem

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/stackandqueue/MainOfCircularTour.java
@@ -0,0 +1,23 @@
+package com.stackandqueue;
+
+public class MainOfCircularTour 
+{
+	
+
+	    public static void main(String[] args) 
+	    {
+
+	        int[] petrol = {6, 3, 7};
+	        int[] distance = {4, 6, 3};
+
+	        int start = CircularTour.findStartingPump(petrol, distance);
+
+	        if (start != -1)
+	            System.out.println("Start at petrol pump index: " + start);
+	        else
+	            System.out.println("No possible circular tour");
+	    }
+	
+
+
+}
