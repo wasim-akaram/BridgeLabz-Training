9c5cc1975337cff02211535d7e6abad3fbb648bb
wasim-akaram <wasimakaram360@gmail.com>
2026-01-05 01:10:17 +0530

    [Wasim] : added implementation of queue using stack

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/stackandqueue/MainOfQueueUsingStack.java
@@ -0,0 +1,23 @@
+package com.stackandqueue;
+
+
+
+public class MainOfQueueUsingStack 
+{
+
+    public static void main(String[] args) 
+    {
+
+        QueueUsingStacks queue = new QueueUsingStacks();
+
+        queue.enqueue(56);
+        queue.enqueue(26);
+        queue.enqueue(98);
+
+        System.out.println("Dequeued: " + queue.dequeue());
+        System.out.println("Front: " + queue.peek());
+        System.out.println("Dequeued: " + queue.dequeue());
+        System.out.println("Dequeued: " + queue.dequeue());
+        System.out.println("Dequeued: " + queue.dequeue());
+    }
+}
---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/stackandqueue/QueueUsingStacks.java
@@ -0,0 +1,65 @@
+package com.stackandqueue;
+
+
+
+import java.util.Stack;
+
+class QueueUsingStacks 
+{
+
+    private Stack<Integer> inStack = new Stack<>();
+    private Stack<Integer> outStack = new Stack<>();
+
+    // Enqueue operation
+    public void enqueue(int data) 
+    {
+        inStack.push(data);
+        System.out.println(data + " enqueued");
+    }
+
+    // Dequeue operation
+    public int dequeue() 
+    {
+        if (isEmpty()) 
+        {
+            System.out.println("Queue is empty");
+            return -1;
+        }
+
+        if (outStack.isEmpty()) 
+        {
+            while (!inStack.isEmpty()) 
+            {
+                outStack.push(inStack.pop());
+            }
+        }
+
+        return outStack.pop();
+    }
+
+    // Peek operation
+    public int peek() 
+    {
+        if (isEmpty()) 
+        {
+            System.out.println("Queue is empty");
+            return -1;
+        }
+
+        if (outStack.isEmpty()) 
+        {
+            while (!inStack.isEmpty()) 
+            {
+                outStack.push(inStack.pop());
+            }
+        }
+
+        return outStack.peek();
+    }
+
+    // Check if queue is empty
+    public boolean isEmpty() 
+    {
+        return inStack.isEmpty() && outStack.isEmpty();
+    }
+}
