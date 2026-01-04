ef3394bf281c6a5e7c03c67f613cd3c472ca6378
wasim-akaram <wasimakaram360@gmail.com>
2026-01-05 00:52:24 +0530

    [Wasim] : added undo redo functionality in text editor app implenting linked list

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/linkedlist/undoredo/TextState.java
@@ -0,0 +1,18 @@
+package com.linkedlist.undoredo;
+
+class TextState 
+{
+	
+	
+    String content;
+    TextState next;
+    TextState prev;
+    
+
+    TextState(String content) 
+    {
+        this.content = content;
+        this.prev = null;
+        this.next = null;
+    }
+}
