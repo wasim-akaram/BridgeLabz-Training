392990273a272fa7e1ea8f3c272cee005c0e98ea
wasim-akaram <wasimakaram360@gmail.com>
2026-01-05 13:09:46 +0530

    [Wasim] : added Stock Span problem

---
+++ java-dsa-practice/gcr-codebase/JavaDsaProgramming/src/com/stackandqueue/stockspan/Main.java
@@ -0,0 +1,29 @@
+package com.stackandqueue.stockspan;
+
+public class Main
+{
+	
+
+	    public static void main(String[] args) 
+	    {
+
+	        int[] price = {120, 90, 65, 71, 48, 57, 5};
+
+	        int[] span = StockSpan.calculateSpan(price);
+
+	        System.out.println("Stock Prices:");
+	        for (int p : price) 
+	        {
+	            System.out.print(p + " ");
+	        }
+
+	        System.out.println("\nStock Span:");
+	        for (int s : span) 
+	        {
+	            System.out.print(s + " ");
+	        }
+	    }
+	
+
+
+}
