2d5b52384fcd60643211c04723cb401027c131b4
wasim-akaram <wasimakaram360@gmail.com>
2026-01-08 12:25:04 +0530

    [Wasim] : added modifications in quick sort

--- java-dsa-practice/gcr-codebase/JavaDsaProgramming/src/com/sortingalgorithms/QuickSort.java
+++ java-dsa-practice/gcr-codebase/JavaDsaProgramming/src/com/sortingalgorithms/QuickSort.java
@@ -1,5 +1,54 @@
 package com.sortingalgorithms;
 
-public class QuickSort {
+import java.util.Arrays;
+import java.util.Scanner;
+public class QuickSort 
+{
+	public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+
+    public static void main(String[] args) 
+    {
+    	Scanner sc = new Scanner(System.in);
+    	System.out.println("enter the number of products");
+    	int numberOfProduct = sc.nextInt();
+    	System.out.println("enter the price of each product");
+    	
+    	int [] arr = new int[numberOfProduct];
+    	
+    	for(int i=0; i< numberOfProduct; i++)
+    	{
+    		arr[i] = sc.nextInt();
+    	}
+      
+    	
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+        
+        sc.close();
+    }
+
 
 }
