61765d79e4bc4bc8e5ee9a791ee264182e154c4a
wasim-akaram <wasimakaram360@gmail.com>
2026-01-10 13:15:03 +0530

    [Wasim] : added User class of arify

--- java-oops-practice/scenario-based-codebase/OopsScenarioBased/src/artify/User.java
+++ java-oops-practice/scenario-based-codebase/OopsScenarioBased/src/artify/User.java
@@ -1,31 +1,32 @@
 package artify;
 
-public class User 
-{
+
+public class User {
 	String name;
-	
+
 	private double walletBalance;
-	
-	public User(String name, double walletBalance)
-	{
+
+	public User(String name, double walletBalance) {
 		this.name = name;
 		this.walletBalance = walletBalance;
 	}
-	
-	//getter for balance
-	public double getBalance()
-	{
+
+	// getter for balance
+	public void showBalance() {
+		System.out.println(Double.toString(walletBalance));
+	}
+
+	// getter for balance
+	public double getBalance() {
 		return walletBalance;
 	}
-	
-	//setter for balance
-	
-	public double setBalance(double amount)
-	{
+
+	// setter for balance
+
+	public double setBalance(double amount) {
 		walletBalance -= amount;
 		return walletBalance;
-		
+
 	}
-	
 
 }
