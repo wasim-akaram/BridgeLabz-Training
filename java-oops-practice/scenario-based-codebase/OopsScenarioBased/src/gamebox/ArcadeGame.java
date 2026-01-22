a7e56dfcd9b690b3dbc9954efb35ca6c59f700de
wasim-akaram <wasimakaram360@gmail.com>
2026-01-22 11:29:33 +0530

    [Wasim] : made some changes in class ArcadeGame

--- java-oops-practice/scenario-based-codebase/OopsScenarioBased/src/gamebox/ArcadeGame.java
+++ java-oops-practice/scenario-based-codebase/OopsScenarioBased/src/gamebox/ArcadeGame.java
@@ -2,16 +2,18 @@ package gamebox;
 
 class ArcadeGame extends Game {
 
-    public ArcadeGame(String title, double price, double rating) {
+    public ArcadeGame(String title, double price, double rating)
+    {
         super(title, "Arcade", price, rating);
     }
 
-    public ArcadeGame(String title, double rating) {
+    public ArcadeGame(String title, double rating) 
+    {
         super(title, "Arcade", rating);
     }
 
     @Override
     public void playDemo() {
-        System.out.println("⚡ Arcade Demo: Fast-paced action for 2 minutes!");
+        System.out.println(" Arcade Demo: Fast-paced action for 2 minutes!");
     }
 }
