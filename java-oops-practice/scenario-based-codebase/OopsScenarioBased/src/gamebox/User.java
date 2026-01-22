bb8e82543f10c9da03b1cf943235d2abb7abe48c
wasim-akaram <wasimakaram360@gmail.com>
2026-01-22 11:30:38 +0530

    [Wasim] : added some changes in User class

--- java-oops-practice/scenario-based-codebase/OopsScenarioBased/src/gamebox/User.java
+++ java-oops-practice/scenario-based-codebase/OopsScenarioBased/src/gamebox/User.java
@@ -7,19 +7,23 @@ class User {
     private String username;
     private List<Game> ownedGames;
 
-    public User(String username) {
+    public User(String username) 
+    {
         this.username = username;
         this.ownedGames = new ArrayList<>();
     }
 
-    public void purchaseGame(Game game) {
+    public void purchaseGame(Game game) 
+    {
         ownedGames.add(game);
         System.out.println(username + " purchased " + game.title);
     }
 
-    public void showLibrary() {
+    public void showLibrary() 
+    {
         System.out.println( username + "'s Game Library:");
-        for (Game g : ownedGames) {
+        for (Game g : ownedGames) 
+        {
             System.out.println("- " + g.title + " (" + g.genre + ")");
         }
     }
