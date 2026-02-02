fa2327321bc54f3309f507b2315b5621bfa77576
wasim-akaram <wasimakaram360@gmail.com>
2026-02-02 11:34:48 +0530

    [Wasim] : modified code of method calling methods with exceptions

--- java-collection-practice/gcr-codebase/JavaCollection/src/com/exceptionHandling/ExceptionTransfer.java
+++ java-collection-practice/gcr-codebase/JavaCollection/src/com/exceptionHandling/ExceptionTransfer.java
@@ -1,5 +1,29 @@
 package com.exceptionHandling;
-
-public class ExceptionTransfer {
+public class ExceptionTransfer
+{
+	public static void method1()
+	{
+		System.out.println(10/0);
+	}
+	
+	
+	
+	public static void method2()
+	{
+		method1();
+	}
+	
+	public static void main(String args[])
+	{
+		try
+		{
+			method2();
+			
+		}
+		catch(ArithmeticException ec)
+		{
+			System.out.println("Arithmetic exception occured: " + ec.getMessage());
+		}
+	}
 
 }
