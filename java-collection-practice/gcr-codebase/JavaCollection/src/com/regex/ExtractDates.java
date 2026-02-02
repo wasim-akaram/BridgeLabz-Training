d8e680e89ce1697f267b0c9c8df9799c18689f8a
wasim-akaram <wasimakaram360@gmail.com>
2026-02-02 13:33:06 +0530

    [Wasim] : added a program to extract dates

---
+++ java-collection-practice/gcr-codebase/JavaCollection/src/com/regex/ExtractDates.java
@@ -0,0 +1,31 @@
+package com.regex;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExtractDates 
+{
+
+	  public static void main(String[] args)
+	    {
+	    	Scanner sc = new Scanner(System.in);
+	    	
+	    	System.out.println("Enter a text to extract dates");
+	    	
+	        String text = sc.nextLine();
+
+	    
+	        String regex ="\\b\\d{2}[-/]\\d{2}[-/]\\d{4}\\b";
+
+
+	        Pattern pattern = Pattern.compile(regex);
+	        Matcher matcher = pattern.matcher(text);
+
+	        while (matcher.find()) 
+	        {
+	            System.out.println(matcher.group());
+	        }
+	    }
+
+}
