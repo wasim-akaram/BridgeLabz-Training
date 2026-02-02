19daa0d1e47bbb90eb7201e66c626079cd02f38e
wasim-akaram <wasimakaram360@gmail.com>
2026-02-02 14:23:23 +0530

    [Wasim] : added a program to remove extra spaces

---
+++ java-collection-practice/gcr-codebase/JavaCollection/src/com/regex/ReplaceSpace.java
@@ -0,0 +1,28 @@
+package com.regex;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ReplaceSpace 
+{
+	public static void main(String[] args)
+    {
+    	Scanner sc = new Scanner(System.in);
+    	
+    	System.out.println("Enter a text to replace space");
+    	
+        String text = sc.nextLine();
+
+    
+        String regex = "\\s+";
+        String result = text.replaceAll("\\s+", " ");
+        
+        System.out.println(result);
+
+
+        
+    }
+
+
+}
