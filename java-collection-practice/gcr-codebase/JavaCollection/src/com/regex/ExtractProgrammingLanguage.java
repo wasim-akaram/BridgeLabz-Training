ddf55f98d0d8415b0a21df94f5e05a5763d2aafd
wasim-akaram <wasimakaram360@gmail.com>
2026-02-02 15:22:15 +0530

    [Wasim] : added a program to extract the names of programming language

---
+++ java-collection-practice/gcr-codebase/JavaCollection/src/com/regex/ExtractProgrammingLanguage.java
@@ -0,0 +1,41 @@
+package com.regex;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+
+public class ExtractProgrammingLanguage 
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		
+		List <String> programminglanguagelist = new ArrayList<>();
+		programminglanguagelist.add("Java");
+		programminglanguagelist.add("C\#");
+		programminglanguagelist.add("C++");
+		programminglanguagelist.add("Solidity");
+		programminglanguagelist.add("Rust");
+		programminglanguagelist.add("SQL");
+		programminglanguagelist.add("Python");
+		
+		//passing the whole list top regex so that it can be matched
+		String regex = String.join("|",programminglanguagelist);
+		
+		Pattern pattern = Pattern.compile(regex);
+		
+		System.out.println("Enter the Text");
+		String text = sc.nextLine();
+		
+		Matcher matcher = pattern.matcher(text);
+		
+		while(matcher.find())
+		{
+			System.out.println(matcher.group());
+		}
+	}
+
+}
