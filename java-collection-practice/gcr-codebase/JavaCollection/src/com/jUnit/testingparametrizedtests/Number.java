80f74f0c0df938888558d91a3427516065b1061e
wasim-akaram <wasimakaram360@gmail.com>
2026-02-03 11:08:53 +0530

    [Wasim] : added a program to test parametrized test

---
+++ java-collection-practice/gcr-codebase/JavaCollection/src/com/jUnit/testingparametrizedtests/Number.java
@@ -0,0 +1,9 @@
+package com.jUnit.testingparametrizedtests;
+
+public class Number 
+{
+	public boolean isEven(int number) 
+	{
+		return number % 2 == 0;
+	}
+}
---
+++ java-collection-practice/gcr-codebase/JavaCollection/src/com/jUnit/testingparametrizedtests/NumberTest.java
@@ -0,0 +1,26 @@
+package com.jUnit.testingparametrizedtests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import com.junit.main.Basic.ParameterizedTest.Number;
+
+class NumberTest 
+{
+	Number obj = new Number();
+
+	@ParameterizedTest
+	@ValueSource(ints = { 2, 4, 6, 8, 10 })
+	void testIsEvenWithEvenNumbers(int number) 
+	{
+		assertTrue(obj.isEven(number));
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = { 1, 3, 5, 7, 9 })
+	void testIsEvenWithOddNumbers(int number) 
+	{
+		assertFalse(obj.isEven(number));
+	}
+}
