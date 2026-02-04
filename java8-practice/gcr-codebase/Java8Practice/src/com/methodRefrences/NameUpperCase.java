9831a831e3420e4afd685e89da61f09ed3f7cd1e
wasim-akaram <wasimakaram360@gmail.com>
2026-02-04 16:06:31 +0530

    [Wasim] : added a program to convert names to upper case

---
+++ java8-practice/gcr-codebase/Java8Practice/src/com/methodRefrences/NameUpperCase.java
@@ -0,0 +1,30 @@
+package com.methodRefrences;
+
+
+	import java.util.*;
+	import java.util.stream.*;
+
+	public class NameUpperCase 
+	{
+
+	    public static void main(String[] args) 
+	    {
+
+	        List<String> employeeNames = Arrays.asList(
+	            "Wasim",
+	            "Akaram",
+	            "Rahul",
+	            "Anita"
+	        );
+
+	
+	        List<String> upperCaseNames = employeeNames.stream()
+	                .map(String::toUpperCase)
+	                .collect(Collectors.toList());
+
+	
+	        upperCaseNames.forEach(System.out::println);
+	    }
+	}
+
+
