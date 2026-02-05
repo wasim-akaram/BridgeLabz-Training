66c3227655c228050a9a0fbe4da83e3e2c44979e
wasim-akaram <wasimakaram360@gmail.com>
2026-02-05 10:36:27 +0530

    [Wasim] : added background job execution

---
+++ java8-practice/gcr-codebase/Java8Practice/src/com/functionalInterface/backgroundJobExecution/BackgroundJob.java
@@ -0,0 +1,13 @@
+package com.functionalInterface.backgroundJobExecution;
+public class BackgroundJob {
+
+    public static void main(String[] args) {
+
+        Runnable job = () -> {
+            System.out.println("Background job running...");
+        };
+
+        Thread t = new Thread(job);
+        t.start();
+    }
+}
